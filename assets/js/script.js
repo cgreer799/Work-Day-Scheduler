4d514d8ce0660ef337f802551111807586712b01
cgreer799 <cgreer799@gmail.com>
2022-10-07 23:04:08 -0500

    updated function in script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,16 @@
 function colorRow(rowNum){
+    let currentMoment = moment();
+    let currentHour = currentMoment.hour();
+    var className = "past-row";
+    if (currentHour == rowNum) {
+        className = "current-row"
+    } else if (currentHour < rowNum) {
+        className = "future-row"
+    }
     let inputField = $("#input-" + rowNum);
-    inputField.addClass("current-row");
+    inputField.addClass(className);
     let inputBackground = $("#input-background-" + rowNum);
-    inputBackground.addClass("current-row");
+    inputBackground.addClass(className);
 }
 
 for (var i = 9; i <= 17; i++) {
